9bf51eec34a9ac22abcc9264f95314ad8a1eb1c0
Jørgen Stamnes <69796724+jorgen-vy@users.noreply.github.com>
2022-02-09 15:19:32 +0100

    Update outputs.tf (default -> value)

--- cross-account-sns-sqs/outputs.tf
+++ cross-account-sns-sqs/outputs.tf
@@ -1,7 +1,7 @@
 output "sqs" {
-  default = aws_sqs_queue.app_sqs
+  value = aws_sqs_queue.app_sqs
 }
 
 output "topic_subscription" {
-  default = aws_sns_topic_subscription.sqs_subscription
+  value = aws_sns_topic_subscription.sqs_subscription
 }
